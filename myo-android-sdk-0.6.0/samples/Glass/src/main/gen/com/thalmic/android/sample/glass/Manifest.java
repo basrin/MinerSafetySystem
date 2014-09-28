36977c9fb7ece41177e315d3a00ee839e84b12be
Taimoorrana1 <taimoorrana1@gmail.com>
2014-09-28 11:38:25 -0400

    Added working project

--- myo-android-sdk-0.6.0/samples/Glass/src/main/gen/com/thalmic/android/sample/glass/Manifest.java
+++
@@ -1,7 +0,0 @@
-/*___Generated_by_IDEA___*/
-
-package com.thalmic.android.sample.glass;
-
-/* This stub is only used by the IDE. It is NOT the Manifest class actually packed into the APK */
-public final class Manifest {
-}
