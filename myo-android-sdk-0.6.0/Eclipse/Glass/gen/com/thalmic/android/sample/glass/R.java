e1b731de9d253f47d43b5a05f66b97b9351ec74f
basrin <b_asrin@hotmail.com>
2014-09-27 20:48:58 -0400

    Myo App

---
+++ myo-android-sdk-0.6.0/Eclipse/Glass/gen/com/thalmic/android/sample/glass/R.java
@@ -0,0 +1,7 @@
+/*___Generated_by_IDEA___*/
+
+package com.thalmic.android.sample.glass;
+
+/* This stub is only used by the IDE. It is NOT the R class actually packed into the APK */
+public final class R {
+}
